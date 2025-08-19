c5438b87d3ec953642c85da0f16cc5e613095ad7
m0hamed-Diaa <mohameddiaaahdahed@gmail.com>
2025-08-19 12:44:06 +0300

    رسالة التعديل

--- js/script.js
+++ js/script.js
@@ -3,17 +3,19 @@ let scrollTop = document.querySelector(".scroll-top-top");
 let navBar = document.querySelector(".navbar");
 
 window.onscroll = () => {
-    if (window.scrollY >= 1000 || window.innerHeight > document.querySelector("#bigSkills").getBoundingClientRect().top) {
+    if (window.scrollY > 1000 || window.innerHeight > document.querySelector("#bigSkills").getBoundingClientRect().top) {
         scrollTop.classList.add("active");
-        navBar.style.opacity = '1';
+        navBar.style.opacity = '.8';
         document.querySelectorAll(".allSkills span").forEach((skill) => {
             skill.style.width = skill.dataset.skill;
         })
     }
     else {
-        scrollTop.classList.remove("active")
+        scrollTop.classList.remove("active");
+        navBar.style.opacity = '1';
     }
 }
+
 // Dark Mode
 window.FontAwesomeConfig = {
     autoAddCss: false,
